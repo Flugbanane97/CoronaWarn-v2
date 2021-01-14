69ac20f9d758a688f465b85ae517cf6cee15e269
Daniel Blümlein <daniel.bluemlein97@outlook.de>
2021-01-14 11:41:40 +0100

    Aufenthalt über db scaffold hinzugefügt

---
+++ Aufenthalt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CoronaWarn
+{
+    public partial class Aufenthalt
+    {
+        public int Id { get; set; }
+        public int BenutzerId { get; set; }
+        public int LocationId { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
--- coronawarnContext.cs
+++ coronawarnContext.cs
@@ -12,73 +12,33 @@ namespace CoronaWarn
         {
         }
 
-        //DB Context using the connection string for the two different tables
         public coronawarnContext(DbContextOptions<coronawarnContext> options)
             : base(options)
         {
         }
 
-        public virtual DbSet<Benutzer> Benutzers { get; set; }
-        public virtual DbSet<Location> Locations { get; set; }
+        public virtual DbSet<Aufenthalt> Aufenthalts { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //laut stackoverflow muss das rausgeschmissen werden
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-//                optionsBuilder.UseSqlServer("Server=tcp:dhbw-devis.database.windows.net,1433;Initial Catalog=corona-warn;Persist Security Info=False;User ID=default;Password=Stuttgart97;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-//            }
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:dhbw-devis.database.windows.net,1433;Initial Catalog=corona-warn;Persist Security Info=False;User ID=default;Password=Stuttgart97;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
 
-            modelBuilder.Entity<Benutzer>(entity =>
+            modelBuilder.Entity<Aufenthalt>(entity =>
             {
-                entity.HasKey(x => x.Id);
+                entity.HasNoKey();
 
-                entity.ToTable("Benutzer");
+                entity.ToTable("Aufenthalt");
 
-                entity.Property(e => e.Bis).HasColumnType("date");
-
-                entity.Property(e => e.Email).HasMaxLength(50);
-
-                entity.Property(e => e.GebDat).HasColumnType("date");
-
-                entity.Property(e => e.Id).ValueGeneratedOnAdd();
-
-                entity.Property(e => e.Nachname)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.Ort)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.Seit).HasColumnType("date");
-
-                entity.Property(e => e.Straße)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.Vorname)
-                    .IsRequired()
-                    .HasMaxLength(50);
-            });
-
-            modelBuilder.Entity<Location>(entity =>
-            {
-                entity.HasKey(x => x.Id);
-
-                entity.ToTable("Location");
-
-                entity.Property(e => e.Bezeichnung)
-                    .IsRequired()
-                    .HasMaxLength(50);
-
-                entity.Property(e => e.Email).HasMaxLength(50);
+                entity.Property(e => e.Datum).HasColumnType("date");
 
                 entity.Property(e => e.Id).ValueGeneratedOnAdd();
             });
